a317758302dff9d62e613745c2677855e3c5a004
Ozan Sozuoz <ozansozuoz@hotmail.com>
2021-01-18 00:01:27 +0300

    update and save note seperated

--- src/components/TextField.vue
+++ src/components/TextField.vue
@@ -7,7 +7,10 @@
       :disabled="isDisabled"
     />
     <textarea v-model="text" :disabled="isDisabled" />
-    <button :disabled="isDisabled" @click="handleClick">Save</button>
+    <div class="button_container">
+      <button @click="addNewNote">New Note</button>
+      <button :disabled="isDisabled" @click="updateNote">Update</button>
+    </div>
   </div>
 </template>
 
@@ -28,50 +31,46 @@ export default {
   methods: {
     ...mapActions(["addNotes", "updateCurrentNote"]),
 
-    handleClick() {
+    addNewNote() {
       const uuid = uuidv4();
 
-      // Save Note
-      if (!this.getCurrentNote) {
-        db.collection("notes")
-          .add({
-            title: this.noteTitle,
-            uuid,
-            categoryID: this.getCurrentCategory,
-          })
-          .then(function () {
-            console.log("Note successfully written!");
-          })
-          .catch(function (error) {
-            console.error("Error writing Note: ", error);
-          });
-
-        this.addNotes({
-          title: this.noteTitle,
-          uuid,
-          category: this.getCurrentCategory,
-        });
-
-        this.updateCurrentNote({
-          title: this.noteTitle,
-          uuid,
-          categoryID: this.getCurrentCategory,
-        });
-      }
-      // Save text
-      if (this.text === "") return;
-      db.collection("texts")
-        .doc(this.getCurrentNote.uuid)
+      db.collection("notes")
+        .doc(uuid)
         .set({
-          text: this.text,
-          noteID: this.getCurrentNote.uuid,
+          title: this.noteTitle,
+          uuid,
           categoryID: this.getCurrentCategory,
+          text: this.text,
         })
         .then(function () {
-          console.log("Text successfully written!");
+          console.log("Note successfully written!");
         })
         .catch(function (error) {
-          console.error("Error writing Text: ", error);
+          console.error("Error writing Note: ", error);
+        });
+
+      this.addNotes({
+        title: this.noteTitle,
+        uuid,
+        category: this.getCurrentCategory,
+      });
+
+      this.updateCurrentNote({
+        title: this.noteTitle,
+        uuid,
+        categoryID: this.getCurrentCategory,
+      });
+    },
+
+    updateNote() {
+      db.collection("notes")
+        .doc(this.getCurrentNote.uuid)
+        .update({ title: this.noteTitle, text: this.text })
+        .then(function () {
+          console.log("NoteTitle successfully written!");
+        })
+        .catch(function (error) {
+          console.error("Error writing NoteTitle: ", error);
         });
     },
   },
@@ -101,21 +100,41 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang='scss' scoped>
 .textfield_container {
   flex: 0.5;
 }
 input {
   border: 1px solid black;
   width: 80%;
+  height: 45px;
+  border-bottom: none;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  padding: 10px;
 }
 textarea {
   border: 1px solid black;
   height: 80%;
   width: 80%;
+  padding: 10px;
 }
 button {
   border: 1px solid black;
-  display: block;
+  padding: 10px 15px;
+  border-radius: 8px;
+  background-color: black;
+  color: white;
+  font-weight: bold;
+
+  &:hover {
+    background-color: rgb(84, 81, 81);
+    cursor: pointer;
+  }
+}
+.button_container {
+  display: flex;
+  width: 40%;
+  justify-content: space-around;
 }
 </style>
