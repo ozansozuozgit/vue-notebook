1e22226cac00cfaf18fbf0276066438f37786d00
Ozan Sozuoz <ozansozuoz@hotmail.com>
2021-02-03 10:44:46 +0300

    add simpler way to delete note

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -8,12 +8,17 @@
           <v-list-item-subtitle v-text="note.text"></v-list-item-subtitle>
         </v-list-item-content>
 
-        <v-list-item-action @click="deleteNote">
+        <v-list-item-action @click="expand = !expand" v-if="!expand">
           <v-icon v-if="!active" color="grey lighten-1">
             mdi-delete-outline
           </v-icon>
 
           <v-icon v-else color="yellow darken-3"> mdi-delete </v-icon>
+        </v-list-item-action>
+        <v-list-item-action @click="expand = !expand" v-if="expand">
+          <v-icon @click="deleteNote"> mdi-checkbox-marked-circle </v-icon>
+
+          <v-icon color="yellow darken-3"> mdi-close-box </v-icon>
         </v-list-item-action>
       </template>
     </v-list-item>
@@ -29,18 +34,16 @@ export default {
   props: {
     note: Object,
   },
+  data: () => {
+    return {
+      expand: false,
+    };
+  },
   methods: {
     selectNote() {
       EventBus.$emit("addNoteToTextField", this.note);
     },
     deleteNote() {
-      if (
-        !confirm(
-          `Are you sure you want to delete the note '${this.note.title}'?`
-        )
-      ) {
-        return;
-      }
       EventBus.$emit("resetTextField");
       EventBus.$emit("removeNoteFromNoteList", this.note.uuid);
 
