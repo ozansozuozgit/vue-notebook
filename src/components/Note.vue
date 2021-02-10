d9cfa38bb84fbe03a4870751558264f5ff211a28
Ozan Sozuoz <ozansozuoz@hotmail.com>
2021-02-10 11:23:33 +0300

    make links clickable when viewing notes

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -14,19 +14,16 @@
         </v-card-subtitle>
       </v-card>
     </template>
-    <template v-slot:default="dialog">
-      <v-card @click="dialog.value = false" outlined>
+    <template>
+      <v-card outlined>
         <v-card-subtitle class="text-subtitle-1 orange--text text--lighten-2"
           >{{ note.title }}
         </v-card-subtitle>
-        <v-card-subtitle v-if="note.text.length < 150" class="text-caption">
+        <v-card-subtitle>
           {{ note.tagList }}
         </v-card-subtitle>
-        <v-card-subtitle v-if="note.text.length < 150" class="text-caption">
-          {{ note.text }}
-        </v-card-subtitle>
-        <v-card-subtitle v-else class="text-caption">
-          {{ note.text.substring(0, 150) + ".." }}
+        <v-card-subtitle>
+          <span v-html="convertedText"></span>
         </v-card-subtitle>
       </v-card>
     </template>
@@ -55,6 +52,16 @@ export default {
       // EventBus.$emit("resetTextField");
       // EventBus.$emit("removeNoteFromNoteList", this.note.uuid);
       // dbService.removeNote(this.note.uuid);
+    },
+  },
+  computed: {
+    convertedText: function () {
+      var urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
+      //var urlRegex = /(https?:\/\/[^\s]+)/g;
+      return this.note.text.replace(urlRegex, function (url, b, c) {
+        var url2 = c == "www." ? "http://" + url : url;
+        return '<a href="' + url2 + '" target="_blank">' + url + "</a>";
+      });
     },
   },
 };
